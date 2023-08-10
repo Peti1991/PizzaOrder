8d7fb7b085e8a316654e0e735e1cd2df6d0bc1cd
Peti1991 <fodor.peter.1991@gmail.com>
2023-08-10 12:09:22 +0200

    backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,95 +3,39 @@ import type { Request, Response } from "express"
 import cors from "cors"
 import fs from "fs/promises"
 import { z } from "zod"
-
+import path from "path"
 const server = express()
 
 server.use(cors())
+server.use(express.json())
 
-type User = {
-  id: number
-  name: string
-  age: number
-}
 
-const parse = (data: string): User[] => data
-    .split("\n")
-    .filter(row => !!row)
-    .map(row => ({
-      id: +row.split(",")[0],
-      name: row.split(",")[1],
-      age: +row.split(",")[2],
-    }))
-
-const stringify = (data: User[]): string => data
-    .map(user => `${user.id},${user.name},${user.age}`)
-    .join("\n")
-
-const QuerySchema = z.object({
+const OrderSchema = z.object({
   name: z.string(),
+  city: z.string(),
+  street: z.string(),
+  houseNumber: z.number(),
+  phoneNumber: z.number(),
+  email: z.string(),
+  margherita: z.number(),
+  formaggi: z.number(),
+  salami: z.number(),
+  tuna: z.number(),
+  hawaii: z.number(),
+  ham: z.number(),
+  chef: z.number(),
+  date: z.string()
 })
 
-// REST API - GET (method) /api/users (path) -> array
-// GET /api/users?name=John   /api/users?age=30&name=John  -> array
-server.get("/api/users", async (req: Request, res: Response) => {
-
-  const userData = await fs.readFile("./database/users.txt", "utf-8")
-  const users = parse(userData)
-
-  const result = QuerySchema.safeParse(req.query)
-  if (!result.success)
-    return res.json(users)
-
-  const query = result.data
-  let filteredUsers = users.filter(user => user.name.includes(query.name))
-
-  res.json(filteredUsers)
-})
-
-// GET /api/users/15 (id!!!!) path variable -> 1 object / HTTP404
-server.get("/api/users/:id", async (req: Request, res: Response) => {
-  const id = +req.params.id
-
-  const userData = await fs.readFile("./database/users.txt", "utf-8")
-  const users = parse(userData)
-  let filteredUser = users.find(user => user.id === id)
-
-  if (!filteredUser)
-    return res.sendStatus(404)
-
-  res.json(filteredUser)
-})
-
-const CreationSchema = z.object({
-  name: z.string(),
-  age: z.string(),
-  id: z.string(),
-})
-
-// POST /api/users - works, but not best practice yet
-server.post("/api/users", async (req: Request, res: Response) => {
-
-  const result = CreationSchema.safeParse(req.query)
+server.post("/api/order", async (req: Request, res: Response) => {
+  
+  const result = OrderSchema.safeParse(req.body)
   if (!result.success)
     return res.sendStatus(400)
+  const newOrder = result.data
 
-  const userData = await fs.readFile("./database/users.txt", "utf-8")
-  const users = parse(userData)
-  users.push({ name: result.data.name, age: +result.data.age, id: +result.data.id })
-  await fs.writeFile("./database/users.txt", stringify(users), "utf-8")
-
-  res.sendStatus(200)
-})
-
-// DELETE /api/users/15 (id!!!!) path variable -> HTTP200
-server.delete("/api/users/:id", async (req: Request, res: Response) => {
-  const id = +req.params.id
-
-  const userData = await fs.readFile("./database/users.txt", "utf-8")
-  const users = parse(userData)
-  let filteredUsers = users.filter(user => user.id !== id)
-
-  await fs.writeFile("./database/users.txt", stringify(filteredUsers), "utf-8")
+  const jsonFilePath = path.join("./database/", `order_${Date.now()}.json`);
+  await fs.writeFile(jsonFilePath, JSON.stringify(newOrder), "utf-8")
 
   res.sendStatus(200)
 })
